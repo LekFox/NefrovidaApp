3d74e7ca72100b5ee6ab7c3c53c3773e8aa0852c
GlinkIsCoding <carlos.espino.m@gmail.com>
2021-05-29 19:28:36 -0500

    Añadir Activity de NumPasos y Refactor Rutina

--- NFApp/app/src/main/java/mx/tec/quiz/Rutina.kt
+++ NFApp/app/src/main/java/mx/tec/quiz/Rutina.kt
@@ -2,41 +2,27 @@ package mx.tec.quiz
 
 import android.content.Context
 import android.content.Intent
-import android.hardware.Sensor
-import android.hardware.SensorEvent
-import android.hardware.SensorEventListener
-import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
-import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_rutina.*
+import mx.tec.quiz.R.layout.activity_rutina
 
-class Rutina : AppCompatActivity(), SensorEventListener {
 
-    private var sensorManager: SensorManager? = null
+class Rutina : AppCompatActivity() {
 
-    private var running = false
-    private var totalSteps = 0f
-    private var previousTotalSteps = 0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_rutina)
+        setContentView(activity_rutina)
 
 
-        loadData()
-        resetSteps()
-
-        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
 
         val button1 = findViewById<Button>(R.id.btn_habRut)
         val button5 = findViewById<Button>(R.id.btn_home01Rut)
 
         button1.setOnClickListener{
 
-            val intent = Intent(this@Rutina, TipsHabitos:: class.java)
+            val intent = Intent(this@Rutina, NumPasos:: class.java)
             startActivity(intent)
             finish()
         }
@@ -50,60 +36,14 @@ class Rutina : AppCompatActivity(), SensorEventListener {
 
     }
 
-    override fun onResume() {
-        super.onResume()
-        running = true
-        val stepSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
 
-        if (stepSensor == null){
-            Toast.makeText(this, "No sensor detected on this device", Toast.LENGTH_SHORT).show()
-        }else{
-            sensorManager?.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_UI)
-        }
 
-    }
 
-    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-    }
 
-    override fun onSensorChanged(event: SensorEvent?) {
-        if (running){
-            totalSteps = event!!.values[0]
-            val currentSteps = totalSteps.toInt() - previousTotalSteps.toInt()
-            tv_stepsTaken.text = ("$currentSteps")
 
-            circularprogress.apply {
-                setProgressWithAnimation(currentSteps.toFloat())
-            }
-        }
-    }
 
-    private fun resetSteps(){
-        tv_stepsTaken.setOnClickListener{
-            Toast.makeText(this, "Long tap to reset steps", Toast.LENGTH_SHORT).show()
-        }
 
-        tv_stepsTaken.setOnLongClickListener {
-            previousTotalSteps = totalSteps
-            tv_stepsTaken.text = 0.toString()
-            saveData()
 
-            true
-        }
-    }
 
-    private fun saveData(){
-        val sharedPreferences = getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
-        val editor = sharedPreferences.edit()
-        editor.putFloat("key1", previousTotalSteps)
-        editor.apply()
-    }
-
-    private fun loadData(){
-        val sharedPreferences = getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
-        val savedNumber = sharedPreferences.getFloat("key1", 0f)
-        Log.d("Rutina", "$savedNumber")
-        previousTotalSteps = savedNumber
-    }
 
 }
